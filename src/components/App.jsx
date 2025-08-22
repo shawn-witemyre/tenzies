c3abeb169ae4f6a20de664015f74722d20089ad9
Shawn Witemyre <switemyre@comcast.net>
2025-08-21 22:17:25 -0400

    Added win condition to game Added confetti component package to game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
+
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
 
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
@@ -44,6 +49,7 @@ export default function App() {
   ));
   return (
     <main>
+      {gameWon && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
@@ -51,7 +57,7 @@ export default function App() {
       </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
-        Roll
+        {gameWon ? "New Game" : "Roll"}
       </button>
     </main>
   );
